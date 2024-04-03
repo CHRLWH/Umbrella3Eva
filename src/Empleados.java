61eadbdd2614beab526e05a5d7c9b3878625d594
Carlos Hernandez <carloshimp@gmail.com>
2024-04-03 13:28:09 +0200

    Atributos creados

---
+++ src/Empleados.java
@@ -0,0 +1,15 @@
+import java.time.LocalDate;
+
+public class Empleados {
+    private String codEmpleado;
+    private DNI dniEmpleado;
+    private String nombre;
+    private String apellido;
+    private String departamento;
+    private double sueldo;
+    private LocalDate fechaNacimiento;
+    private LocalDate fechaContrato;
+    
+
+
+}
