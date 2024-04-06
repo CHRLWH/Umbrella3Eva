efdcee4bd92489491a1a5fd576e2ea9fde67b126
adminaitor <aitorpj93@gmail.com>
2024-04-06 17:58:50 +0200

    añade la generacion de codigoEmpleado con un metodo añade un fichero "src/testFuncionalidades"

--- src/corporacion/Empleados.java
+++ src/corporacion/Empleados.java
@@ -20,7 +20,7 @@ public class Empleados implements paraEmpleado {
     private String nombre;
     private String apellido;
     private String departamento;
-    private double sueldo;
+    private double sueldoAnual;
     private LocalDate fechaNacimiento;
     private LocalDate fechaContrato;
 
@@ -31,14 +31,14 @@ public class Empleados implements paraEmpleado {
     private static double vigencia = 20; //20 por año
     private static int contadorEmpleado = 0; //aumenta con cada instancia
 
-    Empleados (String codEmpleado, Dni dniEmpleado, String nombre, String apellido, String departamento, double sueldo, LocalDate fechaNacimiento, LocalDate fechaContrato){
+    Empleados (Dni dniEmpleado, String nombre, String apellido, String departamento, double sueldoAnual, LocalDate fechaNacimiento, LocalDate fechaContrato){
 
-        this.codEmpleado = codEmpleado;
+        this.codEmpleado = generarCodEmpleado();
         this.dniEmpleado = dniEmpleado;
         this.nombre = nombre;
         this.apellido = apellido;
         this.departamento = departamento;
-        this.sueldo = sueldo;
+        this.sueldoAnual = sueldoAnual;
         this.fechaNacimiento = fechaNacimiento;
         this.fechaContrato = fechaContrato;
 
@@ -68,9 +68,18 @@ public class Empleados implements paraEmpleado {
     }
 
     public String mostrarDatos(){
-        return "Codigo= "+dniEmpleado.getNumeroDNI()+"\nNombre= "+nombre+"\nApellido= "+apellido
-                +"\nDepartamento= "+departamento+"\nAños en la empresa= "+calcularAntiguedadAnios()+"\nEdad= "+ Period.between(fechaNacimiento, LocalDate.now()).getYears()
-                +"\nFecha de contrato= "+ fechaContrato.format(DateTimeFormatter.ofPattern("dd/MMMM/yyyy"));
+        return "Codigo= "+dniEmpleado.getNumeroDNI()+
+                "\nNombre= "+nombre+"\nApellido= "+apellido +
+                "\nDepartamento= "+departamento+
+                "\nAños en la empresa= "+calcularAntiguedadAnios()+
+                "\nEdad= "+ Period.between(fechaNacimiento, LocalDate.now()).getYears() +
+                "\nFecha de contrato= "+ fechaContrato.format(DateTimeFormatter.ofPattern("dd/MMMM/yyyy"));
+    }
 
+    private String generarCodEmpleado () {
+        contadorEmpleado++;
+        String cabecera = "UMBRE";
+        String codigo = String.format("%04d", contadorEmpleado); //añade 0 a la izquierda hasta llegar a 4 dígitos en caso de necesitarlo
+        return cabecera+codigo;
     }
 }
---
+++ src/testFuncionalidades.java
@@ -0,0 +1,7 @@
+import corporacion.Empleados;
+
+public class testFuncionalidades {
+    public static void main(String[] args) {
+        Empleados empleadoTest = new Empleados();
+    }
+}
