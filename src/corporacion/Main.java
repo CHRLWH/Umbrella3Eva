ac18c2ef42e2847c20cb8f8da3d52119446e14f4
adminaitor <aitorpj93@gmail.com>
2024-04-10 23:02:59 +0200

    Metodos 1,3,4,5 y 7
    1- Mostrar todos los empleados (en formato reducido).
    2- Dar de alta un nuevo empleado. => Se solicitarán los datos de un nuevo empleado,
    cumpliendo todas las indicaciones anteriores. Además:
    3- Buscar un empleado por su código. Mostrará todos los datos del empleado.
    4- Buscar todos los empleados de un departamento. Los mostrará en formato reducido.
    5- Borrar un empleado indicando su código.
    6- Subir el sueldo a un empleado. Hay que solicitar el código del empleado y el porcentaje de
    subida.
    7- Mostrar el salario del mes actual de un empleado, introduciendo su código.

--- src/corporacion/Main.java
+++ src/corporacion/Main.java
@@ -5,21 +5,73 @@ import corporacion.Empleados;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
 
         List<Empleados> empleadosActuales = new ArrayList<>();
         List<Empleados> empleadosAntiguos = new ArrayList<>();
-        mostrarEmpleados(empleadosActuales);
+        mostrarEmpleadosReducido(empleadosActuales);
 
-        Empleados emp1 = new Empleados("54481447J", "Carlos","Hernandez","Informatica",
-                40000, LocalDate.of(2002,12,21),LocalDate.of(2023,11,20));
+        Empleados emp1 = new Empleados("54481447J", "Carlos", "Hernandez", "Informatica",
+                40000, LocalDate.of(2002, 12, 21), LocalDate.of(2023, 11, 20));
 
         empleadosActuales.add(emp1);
     }
 
-    private static void mostrarEmpleados(List<Empleados>empleadosActuales) {
-        empleadosActuales.forEach(Empleados::mostrarReducido);
+
+    private static void mostrarEmpleadosReducido(List<Empleados> empleadosList) {
+        empleadosList.forEach(Empleados::mostrarReducido);
+    }
+
+    private static void buscarEmpleadoPorCodigoYMostrarTodo(List<Empleados> empleadosList, String codigoEmpleado) {
+        Empleados miEmpleado = buscarEmpleadoPorCodigo(empleadosList,codigoEmpleado);
+
+        if (miEmpleado == null) System.out.println("[!] No se ha encontrado un empleado con ese código");
+        else miEmpleado.mostrarTodosDatos();
+    }
+
+
+    private static void buscarEmpleadosPorDepartamentoYMostrarReducido(List<Empleados> empleadosList, String departamento) {
+        List<Empleados> empleadosDelDepartamento =
+                empleadosList.stream().filter(i -> departamento.equals(i.getDepartamento())) //filtrado por departamento
+                .collect(Collectors.toList());
+
+        if (empleadosDelDepartamento.isEmpty())
+            System.out.println("[!] No se han encontrado empleados del departamento de " + departamento);
+        else empleadosDelDepartamento.forEach(Empleados::mostrarReducido);
+    }
+
+    private static void mostrarSalarioEmpleadoPorCodigo(List<Empleados> empleadosList, String codigoEmpleado) {
+        Empleados miEmpleado = buscarEmpleadoPorCodigo(empleadosList,codigoEmpleado);
+
+        if (miEmpleado == null) System.out.println("[!] No se ha encontrado un empleado con ese código");
+        else {
+            System.out.print("El salario del mes de " + miEmpleado.getNombre() + " " + miEmpleado.getApellido() + " es de -> ");
+            miEmpleado.calcularSueldoMensual(true);
+        }
+    }
+    private static void borrarEmpleadoPorCodigo(List<Empleados> empleadosList,List<Empleados> empleadosAntiguosList, String codigoEmpleado) {
+        Empleados miEmpleado = buscarEmpleadoPorCodigo(empleadosList,codigoEmpleado); //
+        if (miEmpleado != null){
+            empleadosAntiguosList.add(miEmpleado);
+            empleadosList.remove(miEmpleado);
+        } else System.out.println("[!] No se ha encontrado un empleado con ese código");
+    }
+    //FUNCIONALIDADES INTERNAS
+
+    /**
+     * Busca un empleado en una lista según el código proporcionado
+     * @param empleadosList lista en la que buscar al empleado
+     * @param codigoEmpleado codigo del empleado a buscar
+     * @return devuelve un empleado o null si no se ha encontrado
+     */
+    private static Empleados buscarEmpleadoPorCodigo(List<Empleados> empleadosList, String codigoEmpleado) {
+        return empleadosList.stream()
+                .filter(i -> codigoEmpleado.equals(i.getCodEmpleado())) //compara el código proporcionado con el código del empleado
+                .findFirst() //se detiene al encontrar el primero
+                .orElse(null); //si no lo encuentra devuelve null
     }
 }
