f13eba3d27a730d3865eeb04295e7fa8820b4b07
Carlos Hernandez <carloshimp@gmail.com>
2024-04-10 09:47:35 +0200

    Changes

--- src/utilidades/paraEmpleado.java
+++ src/utilidades/paraEmpleado.java
@@ -1,6 +1,8 @@
 package utilidades;
 
 public interface paraEmpleado {
+    float calcularSueldo();
+
     public double calcularSueldoMensual(boolean mostrar);
     public int calcularAntiguedadAnios();
     public boolean esteMesCumpleAniosEmpleado();
